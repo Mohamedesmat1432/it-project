54cee9ab795e2cfdaee7114b2166d35a864041b6
Kareem Naguib <kareemn@shmff.gov.eg>
2024-02-06 11:58:52 +0200

    add backup database command

--- app/Console/Commands/DatabaseBackup.php
+++ app/Console/Commands/DatabaseBackup.php
@@ -32,7 +32,7 @@ class DatabaseBackup extends Command
         }
 
         $filename = 'backup-' . Carbon::now()->format('Y-m-d') . '.gz';
-        $command = 'mysqldump --user=' . env('DB_USERNAME') . ' --password=' . env('DB_PASSWORD') . ' --host=' . env('DB_HOST') . ' ' . env('DB_DATABASE') . '  | gzip > ' . storage_path() . 'backup/' . $filename;
+        $command = 'mysqldump --user=' . env('DB_USERNAME') . ' --password=' . env('DB_PASSWORD') . ' --host=' . env('DB_HOST') . ' ' . env('DB_DATABASE') . '  | gzip > ' . storage_path() . '/app/backup/' . $filename;
         $returnVar = null;
         $output = null;
         exec($command, $output, $returnVar);
